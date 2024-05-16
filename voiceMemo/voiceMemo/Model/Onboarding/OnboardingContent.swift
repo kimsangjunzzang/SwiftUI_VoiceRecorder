4c1cbf241b7816e594246350e003612fb3e5eaf2
김상준 <84498457+kimsangjunzzang@users.noreply.github.com>
2024-05-17 02:47:07 +0900

    onBoarding Screen View
    온보딩 화면 구현

--- voiceMemo/voiceMemo/Model/Onboarding/OnboardingContent.swift
+++ voiceMemo/voiceMemo/Model/Onboarding/OnboardingContent.swift
@@ -5,5 +5,15 @@
 
 import Foundation
 
-struct OnboardingContent {
+struct OnboardingContent: Hashable {
+    var imageFileName: String
+    var title: String
+    var subTitle : String
+    
+    init(imageFileName: String, title: String, subTitle: String) {
+        self.imageFileName = imageFileName
+        self.title = title
+        self.subTitle = subTitle
+    }
+    
 }
